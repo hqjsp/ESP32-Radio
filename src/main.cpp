2360e9e7d220a8302553ce1bfa55c43ba3ba54b2
hqjsp <79131757+hqjsp@users.noreply.github.com>
2026-01-06 12:57:15 +1300

    Add files via upload
    Converted project to PlatformIO project for VSCode.

---
+++ src/main.cpp
@@ -0,0 +1,34 @@
+#include <Arduino.h>
+
+#include <LiquidCrystal_I2C.h>
+#include "FMLCD.h"
+
+//Application app = Application();
+
+LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 20, 4);
+MainScreen screen = MainScreen(&lcd, 9990);
+
+void setup() {
+  // put your setup code here, to run once:
+
+  Serial.begin(115200);
+  Serial.println("Hello World!");
+
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
+
+  screen.setRDS(true);
+  screen.setRdsPS("Test");
+  screen.setRdsRT("This is test radiotext for the ESP radio");
+
+  screen.init();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(250);
+
+  screen.tick();
+
+}
